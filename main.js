535a68480c55605e10ffd82eb1263c6fe787f369
justanotheridentity <a.chiffoleau@rubika-edu.com>
2020-01-29 18:35:06 +0100

    Links change

--- main.js
+++ main.js
@@ -3,11 +3,3 @@ $(document).ready(function(){
     $('.header').height($(window).height());
   })
 
-(function ($, window, document, undefined) {
-
-    $(function () {
-      $("#header").load("header.html");
-      $("#footer").load("footer.html");
-    });
-  
-  })(jQuery, window, document);
